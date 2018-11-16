c095a801abb697b7b1709ec3cbd57d7e12aa15b7
rachidox <rachid.el.habbari@gmail.com>
2018-11-16 01:14:43 -0500

    File utils exists

--- src/utils/FileUtils.ts
+++ src/utils/FileUtils.ts
@@ -1,13 +1,23 @@
-import { readFile } from 'fs';
+import { readFile, exists } from 'fs';
 
 export class FileUtils {
   static async readFile(path: string): Promise<string> {
     return new Promise<string>((r, j) => {
-      FileUtils.readFilePromiseImpl(path, r, j);
+      FileUtils.readFileImpl(path, r, j);
     });
   }
 
-  private static readFilePromiseImpl(path: string, resolve: Function, reject: Function) {
+  static async exists(path: string): Promise<boolean> {
+    return new Promise<boolean>((r, j) => {
+      FileUtils.existsImpl(path, r, j);
+    });
+  }
+  
+  static existsImpl(path: string, resolve: Function, reject: Function) {
+    exists(path, exists => resolve(exists));
+  }
+
+  private static readFileImpl(path: string, resolve: Function, reject: Function) {
     readFile(path, (err, data) => {
       if (err) {
         reject(err);
