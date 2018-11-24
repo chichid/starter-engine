f3aae8eb7eb79927c3949e97c14f6205b8a07409
rachidox <rachid.el.habbari@gmail.com>
2018-11-23 21:16:59 -0500

    core di utils

---
+++ core/di/utils.spec.ts
@@ -0,0 +1,19 @@
+import { setProperty, getProperty } from "./utils";
+
+describe("utils", () => {
+  it('should set a property in a function', () => {
+    const fixture = function () { };
+    const testValue = "value", testKey = "myprop";
+    setProperty(fixture, testKey, testValue);
+    const prop = getProperty(fixture, testKey);
+    expect(prop).toBe(testValue);
+  });
+
+  it('should set a property in an object', () => {
+    const fixture = {};
+    const testValue = "value", testKey = "myprop";
+    setProperty(fixture, testKey, testValue);
+    const prop = getProperty(fixture, testKey);
+    expect(prop).toBe(testValue);
+  });
+});
