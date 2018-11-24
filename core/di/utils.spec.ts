5fd9b8300b581adcdd2ed5b3c4e20184596521e6
rachidox <rachid.el.habbari@gmail.com>
2018-11-23 21:42:53 -0500

    fix lint

--- core/di/utils.spec.ts
+++ core/di/utils.spec.ts
@@ -3,7 +3,8 @@ import { setProperty, getProperty } from "./utils";
 describe("utils", () => {
   it('should set a property in a function', () => {
     const fixture = function () { };
-    const testValue = "value", testKey = "myprop";
+    const testValue = "value";
+    const testKey = "myprop";
     setProperty(fixture, testKey, testValue);
     const prop = getProperty(fixture, testKey);
     expect(prop).toBe(testValue);
@@ -11,7 +12,8 @@ describe("utils", () => {
 
   it('should set a property in an object', () => {
     const fixture = {};
-    const testValue = "value", testKey = "myprop";
+    const testValue = "value";
+    const testKey = "myprop";
     setProperty(fixture, testKey, testValue);
     const prop = getProperty(fixture, testKey);
     expect(prop).toBe(testValue);
