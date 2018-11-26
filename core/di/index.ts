cabf5d709081cb03697e090428bc69d726e9488d
Rachid El Habbari <rachid@ca.ibm.com>
2018-11-26 00:47:04 -0500

    fix lint

--- core/di/index.ts
+++ core/di/index.ts
@@ -1,4 +1,4 @@
 export { Module } from "./Module";
 export { Injectable } from "./Injectable";
 export { Inject } from "./Inject";
-export { Injector } from './Injector';
+export { Injector } from "./Injector";
