12680b058b492ccf9c35b3de4946b7c08b6f50ca
rachidox <rachid.el.habbari@gmail.com>
2018-11-26 23:03:55 -0500

    File utils coverage

---
+++ src/utils/FileUtils.spec.ts
@@ -0,0 +1,46 @@
+const mockReadFileContent = "Test Content";
+const mockReadFileError = "Error Reading The File";
+const readFile = jest.fn((path, callback) => {
+  if (path === '/some/test/path.txt') {
+    callback(null, mockReadFileContent);
+  } else {
+    callback(mockReadFileError);
+  }
+});
+
+const exists = jest.fn((path, callback) => {
+  if (path === '/some/existing/path.txt') {
+    callback(true);
+  } else {
+    callback(false);
+  }
+});
+
+jest.mock('fs', () => ({
+  readFile,
+  exists
+}));
+
+import { FileUtils } from "./FileUtils";
+import { callbackify } from "util";
+
+describe('FileUtils', () => {
+  it('should read file', async () => {
+    const fu = new FileUtils();
+    const testPath = '/some/test/path.txt';
+    const content = await fu.readFile(testPath);
+    expect(content).toBe(mockReadFileContent);
+  });
+
+  it(`should throw when no file exists`, async () => {
+    const fu = new FileUtils();
+    const testPath = `/wrong.txt`;
+    expect(fu.readFile(testPath)).rejects.toEqual(mockReadFileError);
+  });
+
+  it('should check if file exists', async () => {
+    const fu = new FileUtils();
+    expect(fu.exists('/some/existing/path.txt')).resolves.toEqual(true);
+    expect(fu.exists('/some/non-existing/path.txt')).resolves.toEqual(false);
+  });
+});
