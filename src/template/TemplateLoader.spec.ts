f276ee7a0e5ca9c93ca290945279be5735a4be12
rachidox <rachid.el.habbari@gmail.com>
2018-11-28 00:54:00 -0500

    template loader

--- src/template/TemplateLoader.spec.ts
+++ src/template/TemplateLoader.spec.ts
@@ -1,19 +1,29 @@
-import { TemplateLoader } from "./TemplateLoader";
-import { Module, Injectable, Inject, Injector } from "@core/di";
+import { Inject, Injectable, Injector, Module } from "@core/di";
 import { FileUtils } from "../utils";
+import { TemplateLoader } from "./TemplateLoader";
 
 describe("TemplateLoader", () => {
-  const TEST_PATH = "/some/path" + "test";
+  const TEST_PATH = "/some/path/test/template.hbs";
   const TEST_CONTENT = "some content template {{content}}";
+  const TEST_MODEL = "export function root() {}";
 
 
   @Injectable()
-  class MockFileUtils {
-    async readFile() {
+  class MockFileUtils extends FileUtils {
+    public async readFile(path: string) {
+      if (path && path.indexOf(".model") !== -1) {
+        return TEST_MODEL;
+      }
+
       return TEST_CONTENT;
+    }
+
+    public async exists(path: string) {
+      return path === TEST_PATH;
     }
   }
 
+  // tslint:disable-next-line:max-classes-per-file
   @Module({
     imports: [
       { dependency: FileUtils, cls: MockFileUtils },
@@ -32,5 +42,20 @@ describe("TemplateLoader", () => {
     const template = await loader.loadTemplate(TEST_PATH);
     expect(template).toBeDefined();
     expect(template.content).toEqual(TEST_CONTENT);
+  });
+
+  it("should get a model file", () => {
+    const loader = Injector(TestingModule).create(TemplateLoader);
+    const modelFile = (loader as any).getModelFile("/some/template/path.hbs");
+    expect(modelFile).toEqual("/some/template/path.model.ts");
+  });
+
+  it("should load a model file", async () => {
+    const loader = Injector(TestingModule).create(TemplateLoader);
+    let model = await loader.loadModel("/non/existent/model");
+    expect(model).toBeNull();
+
+    model = await loader.loadModel(TEST_PATH);
+    expect(model).toBeDefined();
   });
 });
--- src/template/TemplateLoader.ts
+++ src/template/TemplateLoader.ts
@@ -1,15 +1,34 @@
 import { Inject, Injectable } from "@core/di";
-import { Template } from "./Template";
 import { FileUtils } from "../utils";
+import { Template } from "./Template";
+
+const MODEL_FILE_EXT = "model.ts";
 
 @Injectable()
 export class TemplateLoader {
   @Inject
   private fileUtils: FileUtils;
 
-  async loadTemplate(path: string): Promise<Template> {
+  public async loadTemplate(path: string): Promise<Template> {
     const templateContent = await this.fileUtils.readFile(path);
     const template = new Template(templateContent, null);
     return template;
+  }
+
+  public async loadModel(templatePath: string) {
+    const modelFile = this.getModelFile(templatePath);
+    const fileExists = await this.fileUtils.exists(modelFile);
+
+    if (fileExists) {
+      return await import(modelFile);
+    }
+
+    return null;
+  }
+
+  private getModelFile(templatePath: string) {
+    const ext = this.fileUtils.ext(templatePath);
+    const fileWithNoExt = templatePath.replace(ext, "");
+    return `${fileWithNoExt}.${MODEL_FILE_EXT}`;
   }
 }
