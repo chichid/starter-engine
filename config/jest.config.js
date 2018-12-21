31d94d79338f3a8d9d4b8a22a00d9919f3860e69
unknown <rachid.el.habbari@gmail.com>
2018-12-21 17:59:45 -0500

    continuous integration

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,31 +1,26 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig');
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require("./tsconfig");
 
 const args = process.argv.slice(2);
 const isWatch = args.indexOf("--watch") !== -1;
 const coverage = args.indexOf("--coverage") !== -1;
 
 let config = {
-  "name": "redux-typescript",
-  "preset": "ts-jest",
-  "rootDir": "../",
-  "moduleNameMapper": pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/config/' }),
-  "roots": [
-    "src",
-    "core"
-  ]
+  name: "redux-typescript",
+  preset: "ts-jest",
+  rootDir: "../",
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/config/"
+  }),
+  roots: ["src", "core"]
 };
 
 if (!isWatch || coverage) {
   config = Object.assign({}, config, {
-    "coverageDirectory": "../reports",
-    "collectCoverage": true,
-    "collectCoverageFrom": [
-      "**/*.{ts,tsx}",
-      "!**/node_modules/**"
-    ]
+    coverageDirectory: "reports",
+    collectCoverage: true,
+    collectCoverageFrom: ["**/*.{ts,tsx}", "!**/node_modules/**"]
   });
 }
 
 module.exports = config;
-
